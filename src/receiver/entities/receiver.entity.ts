357fe7005887d43a24ed7c37605dd34459911c23
Matheus da Guia de Campos <matheus.rj.ig@gmail.com>
2024-06-08 03:51:07 -0300

    feat: create receiver entity

--- src/receiver/entities/receiver.entity.ts
+++ src/receiver/entities/receiver.entity.ts
@@ -1,1 +1,49 @@
-export class Receiver {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+enum DOCUMENT_TYPE {
+    CPF = 'CPF',
+    CNPJ = 'CNPJ',
+}
+
+enum PIX_KEY_TYPE {
+    CPF = 'CPF',
+    CNPJ = 'CNPJ',
+    EMAIL = 'EMAIL',
+    RANDOM = 'RANDOM',
+}
+
+class Document {
+    @Prop({ index: true })
+    type: DOCUMENT_TYPE;
+
+    @Prop({ index: true })
+    value: string;
+}
+
+class PixKey {
+    @Prop()
+    type: PIX_KEY_TYPE;
+
+    @Prop()
+    value: string;
+}
+
+@Schema()
+export class Receiver {
+    @Prop()
+    name: string;
+
+    @Prop()
+    email: string;
+
+    @Prop()
+    document: Document;
+
+    @Prop()
+    pixKey: PixKey;
+}
+
+export type ReceiverDocument = HydratedDocument<Receiver>;
+
+export const ReceiverSchema = SchemaFactory.createForClass(Receiver);
--- src/receiver/receiver.module.ts
+++ src/receiver/receiver.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ReceiverService } from './receiver.service';
 import { ReceiverController } from './receiver.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Receiver, ReceiverSchema } from './entities/receiver.entity';
 
 @Module({
+    imports: [
+        MongooseModule.forFeature([
+            { name: Receiver.name, schema: ReceiverSchema },
+        ]),
+    ],
     controllers: [ReceiverController],
     providers: [ReceiverService],
 })
