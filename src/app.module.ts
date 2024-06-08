0154aa44535df8cb0c3d8261cf0332ded3045765
Matheus da Guia de Campos <matheus.rj.ig@gmail.com>
2024-06-08 13:22:36 -0300

    feat: config cache module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,15 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { validate } from './environment-variables/environment-variables';
 import { ReceiverModule } from './receiver/receiver.module';
+import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
     imports: [
         ConfigModule.forRoot({
             validate,
+            isGlobal: true,
+        }),
+        CacheModule.register({
             isGlobal: true,
         }),
         ReceiverModule,
