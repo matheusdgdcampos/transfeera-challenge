db2fbf6ec6b2dacddf02316d2f1901272f55d154
Matheus da Guia de Campos <matheus.rj.ig@gmail.com>
2024-06-08 19:20:13 -0300

    chore: inject receiver repository

--- src/receiver/receiver.module.ts
+++ src/receiver/receiver.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { ReceiverService } from './receiver.service';
 import { ReceiverController } from './receiver.controller';
 import { DatabaseModule } from '@/database/database.module';
+import { ReceiverRepository } from './repository/receiver.repository';
 
 @Module({
     imports: [DatabaseModule],
     controllers: [ReceiverController],
-    providers: [ReceiverService],
+    providers: [ReceiverService, ReceiverRepository],
 })
 export class ReceiverModule {}
