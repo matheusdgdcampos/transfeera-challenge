0f17116cd94c3dc1795b9ddd884cc4f72285ddce
Matheus da Guia de Campos <matheus.rj.ig@gmail.com>
2024-06-08 19:19:37 -0300

    feat: receiver repository

---
+++ src/receiver/repository/receiver.repository.ts
@@ -0,0 +1,70 @@
+import { PROVIDERS } from '@/commons/enums/providers.enum';
+import { Inject, Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { Db } from 'mongodb';
+import { Receiver } from '../entities/receiver.entity';
+
+@Injectable()
+export class ReceiverRepository implements OnModuleInit {
+    protected logger = new Logger(this.constructor.name);
+
+    constructor(
+        @Inject(PROVIDERS.DATABASE_CONNECTION)
+        protected connection: Db,
+    ) {}
+
+    async onModuleInit() {
+        await this.createIndexes();
+    }
+
+    public async createIndexes() {
+        const [nameIndex, statusIndex, pixKeyTypeIndex, pixKeyValueIndex] =
+            await Promise.all([
+                this.getCollection().createIndex(
+                    {
+                        name: 1,
+                    },
+                    { name: 'name_index' },
+                ),
+                this.getCollection().createIndex(
+                    {
+                        status: 1,
+                    },
+                    { name: 'status_index' },
+                ),
+                this.getCollection().createIndex(
+                    {
+                        'pixKey.type': 1,
+                    },
+                    { name: 'pix_key_type_index' },
+                ),
+                this.getCollection().createIndex(
+                    {
+                        'pixKey.value': 1,
+                    },
+                    { name: 'pix_key_value_index' },
+                ),
+            ]);
+        this.logger.log(
+            `ReceiverRepository initialize and create indexes: [${nameIndex}, ${statusIndex}, ${pixKeyTypeIndex}, ${pixKeyValueIndex}]`,
+        );
+    }
+
+    private getCollection() {
+        const collection = this.connection.collection('receivers');
+        return collection;
+    }
+
+    public async findAll(): Promise<Receiver[]> {
+        const documents = await this.getCollection().find().toArray();
+        const receivers: unknown = documents.map((receiver) => ({
+            ...receiver,
+            id: receiver._id.toString(),
+        }));
+        return receivers as Receiver[];
+    }
+
+    public async count(): Promise<number> {
+        const count = await this.getCollection().countDocuments();
+        return count;
+    }
+}
