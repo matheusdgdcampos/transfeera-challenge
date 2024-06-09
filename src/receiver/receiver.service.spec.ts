b36fc8a6c747b80a2bbecd72f45260dac6ccb3c6
Matheus da Guia de Campos <matheus.rj.ig@gmail.com>
2024-06-09 16:46:30 -0300

    test: update receiver and improve tests organizer

--- src/receiver/receiver.service.spec.ts
+++ src/receiver/receiver.service.spec.ts
@@ -4,6 +4,9 @@ import { InMemoryDatabase } from '@/database/in-memory-database';
 import { TestingModuleFactory } from '@/commons/factory/testing-module-factory';
 import { ReceiverRepository } from './repository/receiver.repository';
 import { createReceiverMock } from './mocks/receiver.mock';
+import { RECEIVER_STATUS, Receiver } from './entities/receiver.entity';
+import { faker } from '@faker-js/faker';
+import { UnprocessableEntityException } from '@nestjs/common';
 
 describe('ReceiverService', () => {
     let moduleRef: TestingModule;
@@ -29,62 +32,130 @@ describe('ReceiverService', () => {
         await moduleRef.close();
     });
 
-    it('should be able to find all receivers', async () => {
-        await Promise.all([
-            receiverService.create(createReceiverMock()),
-            receiverService.create(createReceiverMock()),
-            receiverService.create(createReceiverMock()),
-            receiverService.create(createReceiverMock()),
-            receiverService.create(createReceiverMock()),
-            receiverService.create(createReceiverMock()),
-            receiverService.create(createReceiverMock()),
-            receiverService.create(createReceiverMock()),
-            receiverService.create(createReceiverMock()),
-            receiverService.create(createReceiverMock()),
-            receiverService.create(createReceiverMock()),
-        ]);
-        const receivers = await receiverService.findAll();
-        const receiversPage2 = await receiverService.findAll(2);
-        expect(receivers).toHaveProperty('metadata');
-        expect(receivers).toHaveProperty('data');
-        expect(Array.isArray(receivers.data)).toBeTruthy();
-        expect(receivers.data.length).toBe(10);
-        expect(receiversPage2).not.toContainEqual(receivers);
+    describe('findAll', () => {
+        it('should be able to find all receivers', async () => {
+            await Promise.all([
+                receiverService.create(createReceiverMock()),
+                receiverService.create(createReceiverMock()),
+                receiverService.create(createReceiverMock()),
+                receiverService.create(createReceiverMock()),
+                receiverService.create(createReceiverMock()),
+                receiverService.create(createReceiverMock()),
+                receiverService.create(createReceiverMock()),
+                receiverService.create(createReceiverMock()),
+                receiverService.create(createReceiverMock()),
+                receiverService.create(createReceiverMock()),
+                receiverService.create(createReceiverMock()),
+            ]);
+            const receivers = await receiverService.findAll();
+            const receiversPage2 = await receiverService.findAll(2);
+            expect(receivers).toHaveProperty('metadata');
+            expect(receivers).toHaveProperty('data');
+            expect(Array.isArray(receivers.data)).toBeTruthy();
+            expect(receivers.data.length).toBe(10);
+            expect(receiversPage2).not.toContainEqual(receivers);
+        });
     });
 
-    it('should be able to create new receiver', async () => {
-        await expect(
-            receiverService.create(createReceiverMock()),
-        ).resolves.not.toThrow();
+    describe('create', () => {
+        it('should be able to create new receiver', async () => {
+            await expect(
+                receiverService.create(createReceiverMock()),
+            ).resolves.not.toThrow();
+        });
     });
 
-    it('should be able to delete one or many receivers', async () => {
-        const receiverOne = await receiverService.create(createReceiverMock());
-        const receiverTwo = await receiverService.create(createReceiverMock());
-        const receiverThree =
+    describe('remove', () => {
+        it('should be able to delete one or many receivers', async () => {
+            const receiverOne =
+                await receiverService.create(createReceiverMock());
+            const receiverTwo =
+                await receiverService.create(createReceiverMock());
+            const receiverThree =
+                await receiverService.create(createReceiverMock());
+
+            await expect(
+                receiverService.remove([receiverOne.id]),
+            ).resolves.not.toThrow();
+            let receivers = (await receiverService.findAll()).data;
+            expect(receivers.length).toBe(2);
+            await expect(
+                receiverService.remove([receiverTwo.id, receiverThree.id]),
+            ).resolves.not.toThrow();
+            receivers = (await receiverService.findAll()).data;
+            expect(receivers.length).toBe(0);
+        });
+    });
+
+    describe('search', () => {
+        it('should be able to search receiver', async () => {
+            await receiverRepository.createIndexes();
+            const createdReceiver =
+                await receiverService.create(createReceiverMock());
             await receiverService.create(createReceiverMock());
+            const receiversSearchName = await receiverService.search(
+                createdReceiver.name,
+            );
 
-        await expect(
-            receiverService.remove([receiverOne.id]),
-        ).resolves.not.toThrow();
-        let receivers = (await receiverService.findAll()).data;
-        expect(receivers.length).toBe(2);
-        await expect(
-            receiverService.remove([receiverTwo.id, receiverThree.id]),
-        ).resolves.not.toThrow();
-        receivers = (await receiverService.findAll()).data;
-        expect(receivers.length).toBe(0);
+            expect(receiversSearchName.length).toBe(1);
+            expect(receiversSearchName[0].name).toBe(createdReceiver.name);
+        });
     });
 
-    it('should be able to search receiver', async () => {
-        const createdReceiver =
-            await receiverService.create(createReceiverMock());
-        await receiverService.create(createReceiverMock());
-        const receiversSearchName = await receiverService.search(
-            createdReceiver.name,
-        );
+    describe('update', () => {
+        it('should be able to update receiver with status draft', async () => {
+            const receiver = await receiverService.create(createReceiverMock());
 
-        expect(receiversSearchName.length).toBe(1);
-        expect(receiversSearchName[0].name).toBe(createdReceiver.name);
+            const receiverDraft: Receiver = {
+                ...receiver,
+                status: RECEIVER_STATUS.DRAFT,
+            };
+
+            const receiverToUpdate = createReceiverMock();
+
+            const receiverDraftUpdated = await receiverService.update(
+                receiverDraft.id,
+                receiverToUpdate,
+            );
+
+            expect(receiverDraftUpdated.name).not.toEqual(receiverDraft.name);
+            expect(receiverDraftUpdated.email).not.toEqual(receiverDraft.email);
+            expect(receiverDraftUpdated.document.value).not.toEqual(
+                receiverDraft.document.value,
+            );
+        });
+
+        it('should be able to update receiver with status validated', async () => {
+            const receiver = await receiverService.create(createReceiverMock());
+
+            const receiverDraft: Receiver = {
+                ...receiver,
+                status: RECEIVER_STATUS.VALIDATED,
+            };
+
+            const receiverToUpdate = {
+                ...receiverDraft,
+                email: createReceiverMock().email,
+            };
+
+            const receiverDraftUpdated = await receiverService.update(
+                receiverDraft.id,
+                receiverToUpdate,
+            );
+
+            expect(receiverDraftUpdated.name).toEqual(receiverDraft.name);
+            expect(receiverDraftUpdated.email).not.toEqual(receiverDraft.email);
+            expect(receiverDraftUpdated.document.value).toEqual(
+                receiverDraft.document.value,
+            );
+        });
+
+        it('should not be able to update receiver', async () => {
+            const id = faker.database.mongodbObjectId();
+            const receiverToUpdate = createReceiverMock();
+            await expect(
+                receiverService.update(id, receiverToUpdate),
+            ).rejects.toThrow(UnprocessableEntityException);
+        });
     });
 });
