a64078b92da36488fdd9f4a75a578b56f904e293
Matheus da Guia de Campos <matheus.rj.ig@gmail.com>
2024-06-09 01:51:44 -0300

    feat: create receiver dto

--- src/receiver/dto/create-receiver.dto.ts
+++ src/receiver/dto/create-receiver.dto.ts
@@ -1,1 +1,49 @@
-export class CreateReceiverDto {}
+import {
+    IsEmail,
+    IsEnum,
+    IsOptional,
+    IsString,
+    MaxLength,
+    ValidateNested,
+} from 'class-validator';
+import {
+    DOCUMENT_TYPE,
+    PIX_KEY_TYPE,
+    RECEIVER_STATUS,
+} from '../entities/receiver.entity';
+
+class DocumentDto {
+    @IsEnum(DOCUMENT_TYPE)
+    type: DOCUMENT_TYPE;
+
+    @IsString()
+    value: string;
+}
+
+class PixKeyDto {
+    @IsEnum(PIX_KEY_TYPE)
+    type: PIX_KEY_TYPE;
+
+    @IsString()
+    value: string;
+}
+
+export class CreateReceiverDto {
+    @IsString()
+    name: string;
+
+    @IsEmail()
+    @IsOptional()
+    @MaxLength(255)
+    email: string;
+
+    @IsEnum(RECEIVER_STATUS)
+    @IsOptional()
+    status?: RECEIVER_STATUS = RECEIVER_STATUS.DRAFT;
+
+    @ValidateNested()
+    document: DocumentDto;
+
+    @ValidateNested()
+    pixKey: PixKeyDto;
+}
